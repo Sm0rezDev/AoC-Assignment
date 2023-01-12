bd2973efec623073dc4488103525cf2357250b12
Sm0rezDev <simonw0210@gmail.com>
2023-01-12 13:21:39 +0100

    results

--- 2019/1/AoC_P1.py
+++ 2019/1/AoC_P1.py
@@ -4,4 +4,10 @@ def required_fuel(_input):
     return [floor(int(mass)/3)-2 for mass in _input.splitlines()]
 
 
+if __name__ == '__main__':
+    with open('2019/1/input.data') as f:
+        data = f.read()
+
+    print(sum(required_fuel(data)))
     
+    # [RESULTS]: 3331849
