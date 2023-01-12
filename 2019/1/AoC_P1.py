d510b2fb075916b5984be7dec07c46100843c17e
Sm0rezDev <simonw0210@gmail.com>
2023-01-12 13:29:28 +0100

    results

--- 2019/1/AoC_P1.py
+++ 2019/1/AoC_P1.py
@@ -2,3 +2,12 @@ from math import floor
 
 def required_fuel(_input):
     return [floor(int(mass)/3)-2 for mass in _input.splitlines()]
+
+
+if __name__ == '__main__':
+    with open('2019/1/input.txt') as f:
+        data = f.read()
+
+    print(sum(required_fuel(data)))
+    
+    # [RESULTS]: 3331849
