665e331f925a22d6b038f9b74e06670279bdc51c
Sm0rezDev <simonw0210@gmail.com>
2023-01-12 11:29:40 +0100

    refactoring

--- 2019/1/AoC_P1.py
+++ 2019/1/AoC_P1.py
@@ -1,29 +1,29 @@
 import unittest
 import math
 
-def process_input(_input):
+def required_fuel(_input):
     return [math.floor(int(mass)/3)-2 for mass in _input.splitlines()]
 
 
 class TestProcessInput(unittest.TestCase):
     sample = "12\n14\n1969\n100756"
 
-    def test_process_input(self):
+    def test_required_fuel(self):
         result = [2, 2, 654, 33583]
-        self.assertEqual(process_input(self.sample), result)
+        self.assertEqual(required_fuel(self.sample), result)
 
 
 if __name__ == '__main__':
     with open('2019/1/input.data') as f:
         data = f.read()
 
-    print(sum(process_input(data)))
+    print(sum(required_fuel(data)))
 
 
 """
 [TEST]
 
-test_ProcessInput (2019.1.AoC_P1.TestProcessInput) ... ok
+test_required_fuel (2019.1.AoC_P1.TestProcessInput) ... ok
 
 ----------------------------------------------------------------------
 Ran 1 test in 0.000s
