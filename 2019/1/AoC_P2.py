a2744f300dcfed49615f0aab4078da8a37696fbd
Sm0rezDev <simonw0210@gmail.com>
2023-01-13 02:57:49 +0100

    Test complete

---
+++ 2019/1/AoC_P2.py
@@ -0,0 +1,24 @@
+from math import floor
+
+def operation(mass):
+    res = floor(int(mass)/3) - 2
+    return res
+    
+
+def module_fuel(_input):
+    return [floor(int(x) / 3) - 2 for x in _input.splitlines()]
+
+
+def fuel_need(_input):
+    totalFuel = []
+    for fuel in _input:
+        _fuel_need = fuel
+        totalFuel.append(_fuel_need)
+        while(True):
+            _fuel_need = operation(_fuel_need)
+            if _fuel_need <= 0:
+                break
+            else:
+                totalFuel.append(_fuel_need)
+    return totalFuel
+    
---
+++ 2019/1/test_AoC_P2.py
@@ -0,0 +1,31 @@
+import unittest
+from .AoC_P2 import module_fuel, fuel_need
+
+
+class TestProcessInput(unittest.TestCase):
+    def setUp(self):
+        self.sample = '12\n14\n1969\n100756'
+        self.fuel = module_fuel(self.sample)
+    
+    
+    def test_module_fuel(self):
+        self.assertEqual(module_fuel(self.sample), [2, 2, 654, 33583])
+
+
+    def test_fuel_need(self):
+        self.assertEqual(fuel_need(self.fuel), [
+            2, 2,
+            654, 216, 70, 21, 5,
+            33583, 11192, 3728, 1240, 411, 135, 43, 12, 2
+            ])
+
+"""
+test_fuel_need (2019.1.test_AoC_P2.TestProcessInput) ... ok
+test_module_fuel (2019.1.test_AoC_P2.TestProcessInput) ... ok
+
+----------------------------------------------------------------------
+Ran 2 tests in 0.000s
+
+OK
+"""
+
