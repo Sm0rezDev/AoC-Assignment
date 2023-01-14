a70b8fc5ec74b8819c97828476e997ca0fc23182
Sm0rezDev <simonw0210@gmail.com>
2023-01-14 14:09:59 +0100

    something is borked

---
+++ 2019/2/AoC_2019_D2_P1.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+
+def int_process(int_code):
+    int_code   
+    i = 0
+    while True:
+        if int_code[i] == 1:
+            #int_code[int_code[i+3]] = sum(int_code[int_code[i+j]] for j in [1,2])
+            int_code[int_code[3]] = int_code[int_code[i+1]] + int_code[int_code[i+2]]
+        if int_code[i] == 2:
+            #int_code[int_code[i+3]] = reduce(lambda x, y: x*y, (int_code[int_code[i+j]] for j in [1,2]))
+            int_code[int_code[i+3]] = int_code[int_code[i+1]] * int_code[int_code[i+2]]
+
+        i+=4 # steps forward 4 steps to next opcode.
+        if (int_code[i] == 99): break # Halts when encounter opcode 99.
+    return int_code
+
+
+
+if __name__ == '__main__':
+    pass
