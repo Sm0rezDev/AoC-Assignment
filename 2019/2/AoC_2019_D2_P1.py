93e93b920e2775ab056f3eaf29ff1027712c51c1
Sm0rezDev <simonw0210@gmail.com>
2023-01-15 04:02:21 +0100

    refactoring

--- 2019/2/AoC_2019_D2_P1.py
+++ 2019/2/AoC_2019_D2_P1.py
@@ -1,21 +1,59 @@
 
-from functools import reduce
+class int_code_interpreter:
+
+    def __init__(self, mem):
+        self.mem = mem
 
 
-def int_process(int_code):
-    i = 0
-    while True:
-        if int_code[i] == 1:
-            int_code[int_code[i+3]] = sum(
-                int_code[int_code[i+j]] for j in [1,2])
-        elif int_code[i] == 2:
-            int_code[int_code[i+3]] = reduce(
-                lambda x, y: x*y, (int_code[int_code[i+j]] for j in [1,2]))
+    @staticmethod
+    def _parse(instruct):
+        op_code = instruct % 100
 
-        i+=4 # steps forward 4 steps to next opcode.
-        if int_code[i] == 99:
-            break # Halts when encounter opcode 99.
-    return int_code
+        mode = instruct // 100
+        mode1 = mode % 100
+        mode //= 10
+        mode2 = mode % 10
+        mode //= 10
+        mode3 = mode % 10
+
+        return op_code, mode1, mode2, mode3
+
+
+    def _idx(self, idx, mode):
+        if mode == 0:
+            return self.mem[idx]
+        if mode == 1:
+            return idx
+
+    
+    def execute(self):
+        
+        mem = self.mem
+        idx = self._idx
+        
+        i = 0
+        while True:
+            op_code, mode1, mode2, mode3 = self._parse(mem[i])
+
+            if op_code == 1:
+                #int_code[int_code[i+3]] = sum(int_code[int_code[i+j]] for j in [1,2])
+                p1 = mem[idx(i+1, mode1)]
+                p2 = mem[idx(i+2, mode2)]
+                addr = idx(i+3, mode3)
+                mem[addr] = p1+p2
+                i+=4
+            elif op_code == 2:
+                #int_code[int_code[i+3]] = reduce(lambda x, y: x*y, (int_code[int_code[i+j]] for j in [1,2]))
+                p1 = mem[idx(i+1, mode1)]
+                p2 = mem[idx(i+2, mode2)]
+                addr = idx(i+3, mode3)
+                mem[addr] = p1*p2
+                i+=4
+
+            #i+=4 # steps forward 4 steps to next opcode.
+            if op_code == 99:
+                break # Halts when encounter opcode 99.
+        return mem
 
 
 
@@ -23,8 +61,9 @@ if __name__ == '__main__':
     with open('2019/2/input.txt', encoding='utf8') as f:
         data = list(map(int, f.read().split(',')))
     
-    data[1] = 12
-    data[2] = 2
-
-    print(int_process(data))
+    last_state = [12, 2]
+    for idx, v in enumerate(last_state):
+        data[idx+1] = v
     
+    icp = int_code_interpreter(data)
+    print('Answer:', icp.execute()[0])
--- 2019/2/test_AoC_2019_D2_P1.py
+++ 2019/2/test_AoC_2019_D2_P1.py
@@ -1,22 +1,25 @@
-from AoC_2019_D2_P1 import int_process
+from AoC_2019_D2_P1 import int_code_interpreter
 
-int_code = [
-    [1, 0, 0, 0, 99],
-    [2, 3, 0, 3, 99],
-    [2, 4, 4, 5, 99, 0],
-    [1, 1, 1, 4, 99, 5, 6, 0, 99]
+table = [
+    {
+        'Test': [1, 0, 0, 0, 99],
+        'Expected': [2, 0, 0, 0, 99]
+    },
+    {
+        'Test': [2, 3, 0, 3, 99],
+        'Expected': [2, 3, 0, 6, 99]
+    },
+    {
+        'Test': [2, 4, 4, 5, 99, 0],
+        'Expected': [2, 4, 4, 5, 99, 9801]
+    },
+    {
+        'Test': [1, 1, 1, 4, 99, 5, 6, 0, 99],
+        'Expected': [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    },
 ]
 
-
-def test_one():
-    assert int_process(int_code[0]) == [2, 0, 0, 0, 99]
-
-def test_two():
-    assert int_process(int_code[1]) == [2, 3, 0, 6, 99]
-
-def test_three():
-    assert int_process(int_code[2]) == [2, 4, 4, 5, 99, 9801]
-
-def test_four():
-    assert int_process(int_code[3]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
-
+def test_int_code():
+    for v in table:
+        icp = int_code_interpreter(v['Test'])
+        assert icp.execute() == v['Expected']
